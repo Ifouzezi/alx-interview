6009d35cda195f75a9ba8a3f0ee59fda1d1100d9
ifouzezi <ifoghaleuzezi@gmail.com>
2023-10-11 16:57:58 +0100

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,16 +1,19 @@
-#!/usr/bin/python3
-
-
 def minOperations(n):
-    """
-    Function minOperations
-    Returns an integer
-    """
-    result = 0
-    x = 2
-    while n > 1:
-        while n % x == 0:
-            result += x
-            n /= x
-        x += 1
-    return result
+    if n == 1:
+        return 0  # Already have 1 'H', so no operations needed.
+
+    # Initialize an array to store the minimum operations needed for each index.
+    dp = [0] * (n + 1)
+
+    for i in range(2, n + 1):
+        dp[i] = float('inf')  # Initialize to a large value.
+
+        for j in range(1, i):
+            if i % j == 0:  # If i is divisible by j.
+                dp[i] = min(dp[i], dp[j] + i // j)
+
+    return dp[n]
+
+# Example usage:
+n = 9
+print(minOperations(n))  # Output: 6
