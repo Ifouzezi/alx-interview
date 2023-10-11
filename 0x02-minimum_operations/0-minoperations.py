a51296dbd5dcbd031a4aa2a1aefc93f39735a1b2
ifouzezi <ifoghaleuzezi@gmail.com>
2023-10-11 17:03:21 +0100

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,36 +1,13 @@
 def minOperations(n):
-    if n < 2:
-        return n  # 0 operations needed for n = 0 and n = 1
-
-
-    def is_prime(x):
-        if x <= 1:
-            return False
-        for i in range(2, int(x ** 0.5) + 1):
-            if x % i == 0:
-                return False
-        return True
-
-
-    def get_smallest_prime_factor(x):
-        for i in range(2, x + 1):
-            if x % i == 0 and is_prime(i):
-                return i
-
-    operations = 0
-    while n > 1:
-        smallest_prime = get_smallest_prime_factor(n)
-        if smallest_prime == 0:
-            return 0  # Impossible to achieve 'n' H characters
-        while n % smallest_prime == 0:
-            n //= smallest_prime
-        operations += smallest_prime
-    return operations
-
-# Example usage:
-n = 18
-result = minOperations(n)
-if result == 0:
-    print("Impossible to achieve", n, "H characters.")
-else:
-    print("Fewest number of operations to achieve", n, "H characters:", result)
+    result = 0
+    clipboard = 0
+    for i in range(2, n + 1):
+        while n % i == 0:
+            if clipboard == 0:
+                clipboard = i
+                result += 1
+            else:
+                clipboard *= i
+                result += 1
+            n //= i
+    return result
