1f873a5b29706533ca7dee30f3f0a1f66addfe62
ifouzezi <ifoghaleuzezi@gmail.com>
2023-10-17 16:43:58 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,25 +5,17 @@ Task - Script that reads stdin line by line and computes metrics
 
 import sys
 
-
-if __name__ == "__main__":
-    st_code = {"200": 0,
-               "301": 0,
-               "400": 0,
-               "401": 0,
-               "403": 0,
-               "404": 0,
-               "405": 0,
-               "500": 0}
+if __name__ == "__main":
+    st_code = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
     count = 1
     file_size = 0
 
     def parse_line(line):
-        """ Read, parse and grab data"""
+        """ Read, parse, and grab data"""
         try:
             parsed_line = line.split()
             status_code = parsed_line[-2]
-            if status_code in st_code.keys():
+            if status_code in st_code:
                 st_code[status_code] += 1
             return int(parsed_line[-1])
         except Exception:
@@ -31,7 +23,7 @@ if __name__ == "__main__":
 
     def print_stats():
         """print stats in ascending order"""
-        print("File size: {}".format(file_size))
+        print("Total file size: {}".format(file_size))
         for key in sorted(st_code.keys()):
             if st_code[key]:
                 print("{}: {}".format(key, st_code[key]))
